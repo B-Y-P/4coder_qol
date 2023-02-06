cbc8a174d531b45177ce7aa2472051f420e02f6f
BYP <bypan00@gmail.com>
2023-02-06 01:46:12 -0500

    07 - home scrolls view left

---
+++ 4coder_qol_commands.cpp
@@ -0,0 +1,10 @@
+
+CUSTOM_COMMAND_SIG(qol_home)
+CUSTOM_DOC("[QOL] Seeks the cursor to the beginning of the visual line")
+{
+  View_ID view = get_active_view(app, Access_ReadVisible);
+  seek_pos_of_visual_line(app, Side_Min);
+  Buffer_Scroll scroll = view_get_buffer_scroll(app, view);
+  scroll.target.pixel_shift.x = 0.f;
+  view_set_buffer_scroll(app, view, scroll, SetBufferScroll_NoCursorChange);
+}
