518f7e81f6cde98ffe357bb9578c36b7e807554c
BYP <bypan00@gmail.com>
2023-02-06 19:16:37 -0500

    09 - hot reload themes

--- 4coder_qol.cpp
+++ 4coder_qol.cpp
@@ -48,7 +48,7 @@ void custom_layer_init(Application_Links *app){
     set_custom_hook(app, HookID_BeginBuffer, default_begin_buffer);
     set_custom_hook(app, HookID_EndBuffer, end_buffer_close_jump_list);
     set_custom_hook(app, HookID_NewFile, default_new_file);
-    set_custom_hook(app, HookID_SaveFile, default_file_save);
+    set_custom_hook(app, HookID_SaveFile, qol_file_save);
     set_custom_hook(app, HookID_BufferEditRange, default_buffer_edit_range);
     set_custom_hook(app, HookID_BufferRegion, qol_buffer_region);
     set_custom_hook(app, HookID_ViewChangeBuffer, default_view_change_buffer);
--- 4coder_qol_helper.h
+++ 4coder_qol_helper.h
@@ -3,3 +3,13 @@
 
 function Rect_f32 rect_vsplit(Rect_f32 r, f32 t, b32 c){ return !c ? rect_split_left_right(r, t).min : rect_split_left_right_neg(r, t).max; }
 function Rect_f32 rect_hsplit(Rect_f32 r, f32 t, b32 c){ return !c ? rect_split_top_bottom(r, t).min : rect_split_top_bottom_neg(r, t).max; }
+
+function void
+printf_message(Application_Links *app, char *format, ...){
+  Scratch_Block scratch(app);
+  va_list args;
+  va_start(args, format);
+  String_Const_u8 result = push_stringfv(scratch, format, args);
+  print_message(app, result);
+  va_end(args);
+}
