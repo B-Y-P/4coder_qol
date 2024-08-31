c6ee21eed917180f0509585b63efb3ba55c9582f
BYP <bypan00@gmail.com>
2024-08-31 01:21:49 -0400

    15 - reloading helpers

--- 4coder_qol_commands.cpp
+++ 4coder_qol_commands.cpp
@@ -100,3 +100,47 @@ CUSTOM_DOC("[QOL] Reloads the bindings.4coder file")
     qol_setup_essential_mapping(&framework_mapping, global_map_id, file_map_id, code_map_id);
   }
 }
+
+CUSTOM_COMMAND_SIG(qol_reopen_all_buffers)
+CUSTOM_DOC("[QOL] Reloads the project.4coder file")
+{
+  View_ID views[32];
+  Buffer_Scroll scrolls[32];
+  i64 count = 0;
+
+  for (View_ID view = get_view_next(app, 0, Access_Always);
+       view != 0;
+       view = get_view_next(app, view, Access_Always)){
+    i64 idx = count++;
+    views[idx] = view;
+    scrolls[idx] = view_get_buffer_scroll(app, view);
+  }
+
+  for (Buffer_ID buffer = get_buffer_next(app, 0, Access_Always);
+       buffer != 0;
+       buffer = get_buffer_next(app, buffer, Access_Always)){
+    if (buffer_has_name_with_star(app, buffer)){ continue; }
+    buffer_reopen(app, buffer, 0);
+  }
+
+  for (i64 i = 0; i < count; i += 1){
+    view_set_buffer_scroll(app, views[i], scrolls[i], SetBufferScroll_NoCursorChange);
+  }
+}
+
+CUSTOM_COMMAND_SIG(qol_format_all_buffers)
+CUSTOM_DOC("[QOL] Auto-indent and remove blank lines for all loaded buffers")
+{
+  b32 auto_indent = def_get_config_b32(vars_save_string_lit("automatically_indent_text_on_save"));
+  b32 is_virtual = def_get_config_b32(vars_save_string_lit("enable_virtual_whitespace"));
+  for (Buffer_ID buffer = get_buffer_next(app, 0, Access_ReadWrite);
+       buffer != 0;
+       buffer = get_buffer_next(app, buffer, Access_ReadWrite)){
+    if (buffer_has_name_with_star(app, buffer)){ continue; }
+    if (auto_indent && is_virtual){
+      auto_indent_buffer(app, buffer, buffer_range(app, buffer));
+    }
+    clean_all_lines_buffer(app, buffer, CleanAllLinesMode_RemoveBlankLines);
+  }
+  save_all_dirty_buffers(app);
+}
